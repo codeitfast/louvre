61cc62f921f1200b73f3bd1145b3c88d32ffcff2
codeitfast <74981041+codeitfast@users.noreply.github.com>
2022-12-08 02:34:08 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,25 @@
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+    pkgs.npx tailwindcss init
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
+  ];
+}
